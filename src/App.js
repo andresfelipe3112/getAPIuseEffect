53cfae9d7041dbeed07d4e26275876972e381a13
Andres Felipe <andresfelipedesarrollador@gmail.com>
2021-06-30 01:23:38 -0500

    fist commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import { useEffect, useState } from "react";
 import './App.css';
 
 function App() {
+
+  const [data, setData] = useState("")
+
+
+  useEffect(() => {
+
+    (async function getAPI() {
+      const url = `https://gist.githubusercontent.com/carmandomx/3d7ac5f15af87a587e1d25f5ba96de61/raw/e2d848b87c730a580077de221666343c15b1a243/gistfile1.txt`;
+      const respuesta = await fetch(url);
+      const json = await respuesta.json();
+      setData(json)
+    })();
+
+  }, [])
+
+
+  useEffect(() => {
+    if (data) {
+      console.log(data["quotes"]);
+    }
+  }, [data])
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className="">Click</button>
     </div>
   );
 }
